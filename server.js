123d494fe28dea80a6cb9160b63f2c4008681b9d
mchaudhury <mushsalcha@gmail.com>
2021-09-28 12:33:18 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/index.html"));
+});
+
+const port = process.env.PORT || 5656;
+
+app.listen(port, () =>
+  console.log(`Hippity Hoppity your server is opening on port: ${port}`)
+);
